5d564db1794e842851d76b923135e22f32b50f5f
AlyssaBae <yoonjibae1997@gmail.com>
2021-07-30 14:37:43 -0500

    added some parts but we decided to not use this file

--- postmain.py
+++ postmain.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for  # For flask implementation
+from pymongo import MongoClient
 from datetime import datetime
 
 app = Flask(__name__)
 
-
+client = MongoClient("mongodb://")  # host uri
+db = client.mymongodb  # Select the database
 
 
 if __name__ == "__main__":
